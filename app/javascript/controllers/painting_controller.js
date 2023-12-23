37857c116571927ea1248c95ec1ef8adc91cc769
Stepan Chebannyi <stp.eternal@gmail.com>
2023-12-23 20:37:54 +0100

    show cursor

--- app/javascript/controllers/painting_controller.js
+++ app/javascript/controllers/painting_controller.js
@@ -1,7 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 const MAX_SCALE = 30
+const CURSOR_MIN_SIZE = 5
 const CONTENT_SELECTOR = "#paintingContent"
+const CURSOR_SELECTOR = "#paintingCursor"
 
 export default class extends Controller {
   initialize() {
@@ -10,12 +12,27 @@ export default class extends Controller {
     this.contentOffsetX = 0
     this.contentOffsetY = 0
     this.contentElem = this.element.querySelector(CONTENT_SELECTOR)
-    this.origWidth = this.contentElem.width
-    this.origHeight = this.contentElem.height
+    this.canvasElem = this.contentElem.querySelector('canvas')
+    this.cursorElem = this.element.querySelector(CURSOR_SELECTOR)
+    this.cursorPos = { row: 0, col: 0 }
+    this.origWidth = this.canvasElem.width
+    this.origHeight = this.canvasElem.height
 
     this._initImage()
 
     this._renderContet()
+  }
+
+  moveCursor(event) {
+    if (!this._cursorVisible()) {
+      return
+    }
+
+    let pos = this._getPixel(event.offsetX, event.offsetY)
+    if (pos != this.cursorPos) {
+      this.cursorPos = pos
+      this._adjustCursorPosition()
+    }
   }
 
   clickPixel({ params }) {
@@ -56,13 +73,15 @@ export default class extends Controller {
       this.scale--
     }
 
+    this._adjustCursor()
+
     if (this.scale != oldScale) {
       this._renderContet()
     }
   }
 
   _initImage() {
-    let ctx = this.contentElem.getContext("2d");
+    let ctx = this.canvasElem.getContext("2d");
     this.imgData = ctx.createImageData(this.origWidth, this.origHeight)
     for (let i = 0; i < this.imgData.data.length; i += 4) {
       let row = Math.floor(i / 4 / this.origWidth)
@@ -86,17 +105,46 @@ export default class extends Controller {
   }
 
   _renderContet() {
-    this.contentElem.width = this.scale * this.origWidth
-    this.contentElem.height = this.scale * this.origHeight
+    this.canvasElem.width = this.scale * this.origWidth
+    this.canvasElem.height = this.scale * this.origHeight
 
-    let ctx = this.contentElem.getContext("2d");
+    let ctx = this.canvasElem.getContext("2d");
 
     let bitMap = createImageBitmap(this.imgData, {
-      resizeWidth: this.contentElem.width,
-      resizeHeight: this.contentElem.height,
+      resizeWidth: this.canvasElem.width,
+      resizeHeight: this.canvasElem.height,
       resizeQuality: "pixelated"
     }).then(bitMap => {
-      ctx.drawImage(bitMap, 0, 0, bitMap.width, bitMap.height, 0, 0, this.contentElem.width, this.contentElem.height)
+      ctx.drawImage(bitMap, 0, 0, bitMap.width, bitMap.height, 0, 0, this.canvasElem.width, this.canvasElem.height)
     })
+  }
+
+  _adjustCursor() {
+    let size = this.scale - 2
+    this.cursorElem.style.width = size + 'px'
+    this.cursorElem.style.height = size + 'px'
+    let visibility = 'hidden'
+    if (this._cursorVisible()) {
+      visibility = 'visible'
+    }
+    this.cursorElem.style.visibility = visibility
+
+    this._adjustCursorPosition()
+  }
+
+  _adjustCursorPosition() {
+    this.cursorElem.style.top = (this.scale * this.cursorPos.row + 1) + 'px'
+    this.cursorElem.style.left = (this.scale * this.cursorPos.col + 1) + 'px'
+  }
+
+  _cursorVisible() {
+    return this.scale >= CURSOR_MIN_SIZE + 2
+  }
+
+  _getPixel(offsetX, offsetY) {
+    let row = Math.floor(offsetY / this.scale)
+    let col = Math.floor(offsetX / this.scale)
+
+    return { row, col }
   }
 }
